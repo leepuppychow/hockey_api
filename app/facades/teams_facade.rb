7b1ffb9da75943118a2ffac16d6c02fab80db998
Lee Chow <leepuppychow@gmail.com>
2021-06-20 09:55:58 -0600

    Fixes TeamsFacade by using find_or_initialize_by

--- app/facades/teams_facade.rb
+++ app/facades/teams_facade.rb
@@ -2,15 +2,15 @@ class TeamsFacade
     def self.teams
         json = NhlStatsapiService.get_teams
         teams = json[:teams].map do |team_json|
-            team = Team.create(
-                id: team_json[:id],
-                name: team_json[:name],
-                abbr: team_json[:abbreviation],
-                external_url: NhlStatsapiService.base_url + team_json[:link],
-                division: team_json[:division][:name],
-                conference: team_json[:conference][:name],
-                inaugural_year: team_json[:firstYearOfPlay],
-            )
+            team = Team.find_or_initialize_by(id: team_json[:id])
+            team.name = team_json[:name]
+            team.abbr = team_json[:abbreviation]
+            team.external_url = NhlStatsapiService.base_url + team_json[:link]
+            team.division = team_json[:division][:name]
+            team.conference = team_json[:conference][:name]
+            team.inaugural_year = team_json[:firstYearOfPlay]
+            team.save
+            team
         end
     end
 end
