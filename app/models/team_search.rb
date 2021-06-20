ad7c055687d61f264beb79fef607dab0b73f08bb
Lee Chow <leepuppychow@gmail.com>
2021-06-20 13:37:23 -0600

    Adds TeamSearch model validation

--- app/models/team_search.rb
+++ app/models/team_search.rb
@@ -1,4 +1,4 @@
 class TeamSearch < ApplicationRecord
-  validates_presence_of :name
   validates_presence_of :frequency
+  validates_uniqueness_of :name, scope: [:abbr, :division, :conference], case_sensitive: false
 end
