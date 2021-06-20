7ddecf837a0849fa9f720bf1634f4aceb071ec99
Lee Chow <leepuppychow@gmail.com>
2021-06-20 17:21:16 -0600

    Implements filtering logic for a team's roster of players

--- app/controllers/api/v1/players_controller.rb
+++ app/controllers/api/v1/players_controller.rb
@@ -6,6 +6,10 @@ class Api::V1::PlayersController < ApplicationController
             @team.players = TeamsFacade.get_roster(@team)
         end
         players = @team.players
+            .where(first_name_filter)
+            .where(last_name_filter)
+            .where(position_filter)
+            .where(jersey_filter)
             .order(sort_mappings[player_params[:sort]])
             .all
 
@@ -14,7 +18,7 @@ class Api::V1::PlayersController < ApplicationController
 
     private
         def player_params
-            params.permit(:team_abbr, :sort)
+            params.permit(:team_abbr, :sort, :first_name, :last_name, :position, :jersey)
         end
 
         def find_team
@@ -22,6 +26,22 @@ class Api::V1::PlayersController < ApplicationController
             if not @team
                 render json: {:error => "Cannot find team #{player_params[:team_abbr]}"}, status: 404
             end
+        end
+
+        def first_name_filter
+            ['first_name ILIKE ?', "%#{player_params[:first_name].strip.downcase}%"] unless player_params[:first_name].blank?
+        end
+
+        def last_name_filter
+            ['last_name ILIKE ?', "%#{player_params[:last_name].strip.downcase}%"] unless player_params[:last_name].blank?
+        end
+
+        def position_filter
+            ['lower(position) = ?', player_params[:position].strip.downcase] unless player_params[:position].blank?
+        end
+
+        def jersey_filter
+            ['jersey_number = ?', player_params[:jersey]] unless player_params[:jersey].blank?
         end
 
         def sort_mappings
