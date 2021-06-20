3848b930a8016a1aa9b3bccc6e496d18be43d4a5
Lee Chow <leepuppychow@gmail.com>
2021-06-20 17:38:38 -0600

    Adds validation on RosterSearch having unique combination of search terms

--- app/models/roster_search.rb
+++ app/models/roster_search.rb
@@ -1,4 +1,5 @@
 class RosterSearch < ApplicationRecord
   validates_presence_of :team_abbr
   validates_presence_of :frequency
+  validates_uniqueness_of :team_abbr, scope: [:first_name, :last_name, :position, :jersey_number], case_sensitive: false
 end 
--- spec/models/roster_search_spec.rb
+++ spec/models/roster_search_spec.rb
@@ -9,5 +9,16 @@ describe RosterSearch, type: :model do
     it {should_not validate_presence_of(:last_name)}
     it {should_not validate_presence_of(:position)}
     it {should_not validate_presence_of(:jersey_number)}
+    
+    subject {RosterSearch.new(team_abbr: "COL", position: 'C')}
+    it {should validate_uniqueness_of(:team_abbr).scoped_to(:first_name, :last_name, :position, :jersey_number).case_insensitive}
+    
+    it "cannot create RosterSearch with duplicate search terms (case insensitive)" do
+      search_1 = RosterSearch.create(team_abbr: 'COL', last_name: 'Mack')
+      search_2 = RosterSearch.create(team_abbr: 'col', last_name: 'MACK')
+
+      expect(search_1.save).to be(true) 
+      expect(search_2.save).to be(false) 
+    end
   end
 end
