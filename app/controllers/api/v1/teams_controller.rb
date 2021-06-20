c5b9f0bc4042f67ed70d43df56dd1512d1929272
Lee Chow <leepuppychow@gmail.com>
2021-06-20 00:25:41 -0600

    Refactors TeamsController index action using TeamsFacade and NhlStatsapiService

--- app/controllers/api/v1/teams_controller.rb
+++ app/controllers/api/v1/teams_controller.rb
@@ -1,20 +1,6 @@
 class Api::V1::TeamsController < ApplicationController
     def index
-        base_url = "https://statsapi.web.nhl.com"
-        conn = Faraday.new(url: base_url)
-        response = conn.get("/api/v1/teams")
-        json = JSON.parse(response.body, symbolize_names: true)
-        teams = json[:teams].map do |team_json|
-            team = Team.create(
-                id: team_json[:id],
-                name: team_json[:name],
-                abbr: team_json[:abbreviation],
-                external_url: base_url + team_json[:link],
-                division: team_json[:division][:name],
-                conference: team_json[:conference][:name],
-                inaugural_year: team_json[:firstYearOfPlay],
-            )
-        end
+        teams = TeamsFacade.teams
         render json: teams, status: 200, each_serializer: TeamSerializer
     end
 end
