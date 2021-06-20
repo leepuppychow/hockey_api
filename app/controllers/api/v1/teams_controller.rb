aab9bece88e9d20a58a1b243899e8b375d244db0
Lee Chow <leepuppychow@gmail.com>
2021-06-20 11:17:08 -0600

    Adds private methods for filtering team results

--- app/controllers/api/v1/teams_controller.rb
+++ app/controllers/api/v1/teams_controller.rb
@@ -2,6 +2,10 @@ class Api::V1::TeamsController < ApplicationController
     def index
         TeamsFacade.upsert_teams
         teams = Team
+            .where(name_filter)
+            .where(abbr_filter)
+            .where(division_filter)
+            .where(conference_filter)
             .order(sort_mappings[team_params[:sort]])
             .all
         render json: teams, status: 200, each_serializer: TeamSerializer
@@ -10,7 +14,23 @@ class Api::V1::TeamsController < ApplicationController
     private
 
         def team_params
-            params.permit(:sort)
+            params.permit(:sort, :name, :abbr, :division, :conference)
+        end
+
+        def name_filter
+            ['lower(name) = ?', team_params[:name].strip.downcase] unless team_params[:name].blank?
+        end
+
+        def abbr_filter
+            ['lower(abbr) = ?', team_params[:abbr].strip.downcase] unless team_params[:abbr].blank?
+        end
+
+        def division_filter
+            ['division ILIKE ?', "%#{team_params[:division].strip.downcase}%"] unless team_params[:division].blank?
+        end
+
+        def conference_filter
+            ['conference ILIKE ?', "%#{team_params[:conference].strip.downcase}%"] unless team_params[:conference].blank?
         end
 
         def sort_mappings
