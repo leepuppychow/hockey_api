673d5f34540daaff45c7862f6c00e8084bb52ca0
Lee Chow <leepuppychow@gmail.com>
2021-06-20 15:35:58 -0600

    Adds assertion that TeamSearch.name is not required

--- spec/models/team_search_spec.rb
+++ spec/models/team_search_spec.rb
@@ -2,7 +2,9 @@ require "rails_helper"
 
 describe TeamSearch, type: :model do
   describe "validations" do
-    it {should validate_presence_of(:frequency)}  
+    it {should validate_presence_of(:frequency)}
+
+    it {should_not validate_presence_of(:name)} 
     it {should_not validate_presence_of(:abbr)} 
     it {should_not validate_presence_of(:division)} 
     it {should_not validate_presence_of(:conference)}
