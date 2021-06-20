3aa7077d16bd745c0d0595982a9e31f8aa55ecd0
Lee Chow <leepuppychow@gmail.com>
2021-06-20 01:07:05 -0600

    Stubs out network call to NHL Stats Api for teams data

--- spec/requests/api/v1/teams/index_spec.rb
+++ spec/requests/api/v1/teams/index_spec.rb
@@ -2,12 +2,16 @@ require 'rails_helper'
 
 describe "Teams Index" do
     it "can get all existing teams without roster data" do
+        fixture_json = File.read('spec/fixtures/teams_no_roster.json')
+        stub_request(:get, "https://statsapi.web.nhl.com/api/v1/teams").
+            to_return(status: 200, body: fixture_json)
+        
         get "/api/v1/teams"
 
         teams_json = JSON.parse(response.body)
 
         expect(response.status).to eq 200
-        expect(teams_json.count).to eq 32
+        expect(teams_json.count).to eq 3
         team_1_json = teams_json[0]
         expect(team_1_json['id']).to be_a(Integer)
         expect(team_1_json['name']).to be_a(String)
