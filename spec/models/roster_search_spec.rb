82247610819b826818cb5219a0f478b9a21eece6
leepuppychow <leepuppychow@gmail.com>
2021-06-19 19:08:52 -0600

    Adjusts RosterSearch spec to have first and last name now

--- spec/models/roster_search_spec.rb
+++ spec/models/roster_search_spec.rb
@@ -5,7 +5,8 @@ describe RosterSearch, type: :model do
     it {should validate_presence_of(:team_abbr)}
     it {should validate_presence_of(:frequency)}
     
-    it {should_not validate_presence_of(:full_name)}
+    it {should_not validate_presence_of(:first_name)}
+    it {should_not validate_presence_of(:last_name)}
     it {should_not validate_presence_of(:position)}
     it {should_not validate_presence_of(:jersey_number)}
   end
