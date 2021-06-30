3c3086b8485dfcbfcb54685c68c049a258821e32
leepuppychow <leepuppychow@gmail.com>
2021-06-29 22:31:36 -0600

    Is explicit with which serializer to use for TeamSearch index action

--- app/controllers/api/v1/team_searches_controller.rb
+++ app/controllers/api/v1/team_searches_controller.rb
@@ -1,6 +1,6 @@
 class Api::V1::TeamSearchesController < ApplicationController
     def index
         team_searches = TeamSearch.order("frequency DESC").all
-        render json: team_searches, status: 200
+        render json: team_searches, status: 200, each_serializer: TeamSearchSerializer  
     end
 end
