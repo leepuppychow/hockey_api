78f295bbd3d82f0f5ea4a36b80ea5e57b3bef164
leepuppychow <leepuppychow@gmail.com>
2021-06-29 22:10:49 -0600

    Adds has_many relationships on TeamSearch model

--- app/models/team_search.rb
+++ app/models/team_search.rb
@@ -1,4 +1,7 @@
 class TeamSearch < ApplicationRecord
   validates_presence_of :frequency
   validates_uniqueness_of :name, scope: [:abbr, :division, :conference], case_sensitive: false
+
+  has_many :team_search_teams
+  has_many :teams, through: :team_search_teams
 end
