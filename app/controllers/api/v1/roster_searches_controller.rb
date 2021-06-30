fdef304ddc3e1ff54d1df0201f0eb8259fc2d414
leepuppychow <leepuppychow@gmail.com>
2021-06-30 11:47:31 -0600

    Eager loads the RosterSearch.players data to avoid n+1 queries

--- app/controllers/api/v1/roster_searches_controller.rb
+++ app/controllers/api/v1/roster_searches_controller.rb
@@ -1,6 +1,9 @@
 class Api::V1::RosterSearchesController < ApplicationController
     def index
-        roster_searches = RosterSearch.order("frequency DESC").all
+        roster_searches = RosterSearch
+            .includes(:players)
+            .order("frequency DESC")
+            .all
         render json: roster_searches, status: 200, each_serializer: RosterSearchSerializer
     end
 end
